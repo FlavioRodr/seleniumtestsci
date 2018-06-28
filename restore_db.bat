6c666a1b1a136eae09c8b733638575066bc03ee1
ftosta <flaviotosta7@gmail.com>
2018-06-28 08:12:40 -0500

    fixing script to restore DB

--- restore_db.bat
+++ restore_db.bat
@@ -1,4 +1,2 @@
-sqlcmd -U %1 -P %2 -S %3 -Q "exec msdb.dbo.rds_restore_database 
-        @restore_db_name='%4', 
-        @s3_arn_to_restore_from='%5'"
+sqlcmd -U %1 -P %2 -S %3 -Q "exec msdb.dbo.rds_restore_database @restore_db_name='%4', @s3_arn_to_restore_from='%5'"
 pause
