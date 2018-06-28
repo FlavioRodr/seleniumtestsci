0b8c7c9313565a1f5fcab5239ef5f28de5c4dc2a
ftosta <flaviotosta7@gmail.com>
2018-06-28 08:30:23 -0500

    Adding command to delete database before trying to restore from S3

--- restore_db.bat
+++ restore_db.bat
@@ -1,2 +1,2 @@
-sqlcmd -U %1 -P %2 -S %3 -Q "exec msdb.dbo.rds_restore_database @restore_db_name='%4', @s3_arn_to_restore_from='%5'"
+sqlcmd -U %1 -P %2 -S %3 -Q "ALTER DATABASE [%4] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;DROP DATABASE [%4]; exec msdb.dbo.rds_restore_database @restore_db_name='%4', @s3_arn_to_restore_from='%5'"
 pause
